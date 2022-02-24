d76fdf51c9ffb2c9e8c5e30a6cf677fb3de8ffc8
hyjtab <2741521652@qq.com>
2022-02-24 20:58:58 +0800

    feat: login interface

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: config => {
+    config.plugins.push(AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }))
+    config.plugins.push(Components({
+      resolvers: [ElementPlusResolver()],
+    }))
+  },
   transpileDependencies: true
 })
